3288a7ae40fea6998c5c5539b8fc5f1937d8d51a
David Bernstein <S241736>
2019-09-28 13:33:48 -0400

    Add margin range to mini nav and icon callout

--- wp-content/themes/snapsite/partials/blocks/block-iconcallout.php
+++ wp-content/themes/snapsite/partials/blocks/block-iconcallout.php
@@ -16,32 +16,32 @@ if( $callouts ) :
 ?>
     
 
-    <div class="callout-wrapper columns flex-by-<?php echo $count ?>"
-    <?php if ( $margin > 0) : ?>
-    style="margin: <?php echo $margin; ?>px 0px;"
-    <?php endif; ?>
-    > 
-        <div class="row">
+<div class="callout-wrapper columns flex-by-<?php echo $count ?>"
+<?php if ( $margin > 0) : ?>
+style="margin: <?php echo $margin; ?>px auto;"
+<?php endif; ?>
+> 
+    <div class="row">
         <?php while ( have_rows('callouts') ) :
-            echo '<div class="callout-item-wrapper">';
-            the_row();
-            $i++;
-            ?>
-            <div class="callout-head">
-            <!-- Callout Icon -->
-            <?php $icon = get_sub_field('icon');
-            if ($icon) : ?> 
-                <div class="icon">
-                <?php echo wp_get_attachment_image( $icon['ID'], 'full' ); ?>
-                </div>
-            <?php endif; ?>
-            
-            
-            <!-- Callout Title -->
-            <?php $title = get_sub_field('title');
-            if ($title) : ?> 
-                <h3><?php echo $title?></h3>
-            <?php endif; ?>
+        echo '<div class="callout-item-wrapper">';
+        the_row();
+        $i++;
+        ?>
+        <div class="callout-head">
+        <!-- Callout Icon -->
+        <?php $icon = get_sub_field('icon');
+        if ($icon) : ?> 
+        <div class="icon">
+        <?php echo wp_get_attachment_image( $icon['ID'], 'full' ); ?>
+        </div>
+        <?php endif; ?>
+        
+        
+        <!-- Callout Title -->
+        <?php $title = get_sub_field('title');
+        if ($title) : ?> 
+        <h3><?php echo $title?></h3>
+        <?php endif; ?>
 
             <!-- Calllout Content -->
             <div class="callout-content">
@@ -52,22 +52,23 @@ if( $callouts ) :
                 <?php endif; ?>
             <?php endif; ?>
             </div>
-            </div>
-            <?php $link = get_sub_field('cta');
-            if( $link ): 
-                $link_url = $link['url'];
-                $link_title = $link['title'];
-                $link_target = $link['target'] ? $link['target'] : '_self';?>
-                <div class="button-wrapper">
-                    <a class="button" href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>"><?php echo esc_html($link_title); ?></a>
-                </div>
-            <?php endif; ?>
-            </div>
-        <?php endwhile; ?>
-        <?php wp_reset_postdata(); ?>
-    
+
         </div>
+        <?php $link = get_sub_field('cta');
+        if( $link ): 
+        $link_url = $link['url'];
+        $link_title = $link['title'];
+        $link_target = $link['target'] ? $link['target'] : '_self';?>
+        <div class="button-wrapper">
+            <a class="button" href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>"><?php echo esc_html($link_title); ?></a>
+        </div>
+        <?php endif; ?>
+        </div>
+    <?php endwhile; ?>
+    <?php wp_reset_postdata(); ?>
+
     </div>
+</div>
 
 
 <?php endif ?>
--- wp-content/themes/snapsite/partials/blocks/block-mininav.php
+++ wp-content/themes/snapsite/partials/blocks/block-mininav.php
@@ -6,63 +6,63 @@ $margin = get_sub_field('nav_margin');
 
 if( $nav_group ) : ?>
 
-    <div class="mini-nav-wrapper row"
-    <?php if ( $margin > 0) : ?>
-    style="margin: <?php echo $margin; ?>px 0px;"
-    <?php endif; ?>
-    > 
+<div class="mini-nav-wrapper row"
+<?php if ( $margin > 0) : ?>
+style="margin: <?php echo $margin; ?>px auto;"
+<?php endif; ?>
+> 
 
-    <?php while ( have_rows('nav_group') ) :
-            echo '<div class="nav-group-wrapper">';
-            the_row();
-            $i++;
+<?php while ( have_rows('nav_group') ) :
+        echo '<div class="nav-group-wrapper">';
+        the_row();
+        $i++;
+        ?>
+
+        <!-- Group Label -->
+        <?php $link = get_sub_field('group_label');
+        if( $link ): 
+            $link_url = $link['url'];
+            $link_title = $link['title'];
+            $link_target = $link['target'] ? $link['target'] : '_self';
             ?>
-
-            <!-- Group Label -->
-            <?php $link = get_sub_field('group_label');
-            if( $link ): 
-                $link_url = $link['url'];
-                $link_title = $link['title'];
-                $link_target = $link['target'] ? $link['target'] : '_self';
-                ?>
-                <div class="group-label">
-                    <a href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>"><?php echo esc_html($link_title); ?><i class="arrow right"></i></a>
-                </div>
-            <?php endif; ?>
-            
-            <div class="group-content">
-                <!-- Group Image -->
-                <?php $image = get_sub_field('group_image');
-                if ($image) : ?> 
-                    <?php echo wp_get_attachment_image( $image['ID'], 'full' ); ?>
-                <?php endif; ?>
-                
-                <!-- Group Nav Items -->
-                <?php if( have_rows('nav_items') ): ?>
-                <ul class="nav-items">
-                <?php while( have_rows('nav_items') ): the_row();
-            
-                $nav_item_link = get_sub_field('nav_item');
-                if( $nav_item_link ): 
-                    $nav_item_link_url = $nav_item_link['url'];
-                    $nav_item_link_title = $nav_item_link['title'];
-                    $nav_item_link_target = $nav_item_link['target'] ? $nav_item_link['target'] : '_self';
-                    ?>
-                    <li class="nav-item">
-                        <a href="<?php echo esc_url($nav_item_link_url); ?>" target="<?php echo esc_attr($nav_item_link_target); ?>"><?php echo esc_html($nav_item_link_title); ?></a>
-                    </li>
-                <?php endif; ?>
-                    
-                <?php endwhile; ?>
-                </ul>
-            <?php endif; ?>
+            <div class="group-label">
+                <a href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>"><?php echo esc_html($link_title); ?><i class="arrow right"></i></a>
             </div>
+        <?php endif; ?>
+        
+        <div class="group-content">
+            <!-- Group Image -->
+            <?php $image = get_sub_field('group_image');
+            if ($image) : ?> 
+                <?php echo wp_get_attachment_image( $image['ID'], 'full' ); ?>
+            <?php endif; ?>
             
+            <!-- Group Nav Items -->
+            <?php if( have_rows('nav_items') ): ?>
+            <ul class="nav-items">
+            <?php while( have_rows('nav_items') ): the_row();
+        
+            $nav_item_link = get_sub_field('nav_item');
+            if( $nav_item_link ): 
+                $nav_item_link_url = $nav_item_link['url'];
+                $nav_item_link_title = $nav_item_link['title'];
+                $nav_item_link_target = $nav_item_link['target'] ? $nav_item_link['target'] : '_self';
+                ?>
+                <li class="nav-item">
+                    <a href="<?php echo esc_url($nav_item_link_url); ?>" target="<?php echo esc_attr($nav_item_link_target); ?>"><?php echo esc_html($nav_item_link_title); ?></a>
+                </li>
+            <?php endif; ?>
+                
+            <?php endwhile; ?>
+            </ul>
+        <?php endif; ?>
+        </div>
+        
     </div>
     <?php endwhile; ?>
     <?php wp_reset_postdata(); ?>
     
-
+</div>
 
 
 <?php endif ?>
