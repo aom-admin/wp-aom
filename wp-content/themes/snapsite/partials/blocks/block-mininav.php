7494a8021b2c9fa392779f9b56298bda0a5183e7
David Bernstein <S241736>
2019-09-28 10:20:40 -0400

    [AOM-5] mini nav style updates

--- wp-content/themes/snapsite/partials/blocks/block-mininav.php
+++ wp-content/themes/snapsite/partials/blocks/block-mininav.php
@@ -22,7 +22,7 @@ if( $nav_group ) : ?>
                 $link_target = $link['target'] ? $link['target'] : '_self';
                 ?>
                 <div class="group-label">
-                    <a href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>"><?php echo esc_html($link_title); ?></a>
+                    <a href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>"><?php echo esc_html($link_title); ?><i class="arrow right"></i></a>
                 </div>
             <?php endif; ?>
             
